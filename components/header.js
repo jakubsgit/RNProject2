cc6725785f619d6a437c4f6a39e8c36514c69889
Jakub Antczak <jakubantczak.info@gmail.com>
2019-09-09 22:24:02 +0200

    Creating a new app

---
+++ components/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { View, Text } from "react-native";
+
+const header = props => {
+  return (
+    <View>
+        <Text>Cooking Chalange!</Text>
+    </View>
+  )
+};
+
+export default header;
